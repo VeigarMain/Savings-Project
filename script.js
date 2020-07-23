d2135957f2c1fa19b1bb7d697422578105efe62c
Jake Battaglia <jbaked12@gmail.com>
2020-07-23 19:41:24 -0400

    random commit

--- script.js
+++ script.js
@@ -1,26 +1,4 @@
-var questionAndAnswer = {
-    q: "Favorite color",
-    a: "blue",
-    q: "Favorite food",
-    a: "Mexican",
-    q: "Favorite car",
-    a: "Lambo",
-    q: "Favorite hobby",
-    a: "crafting",
-    q: "Favorite movie",
-    a: "Donnie Narko",
-    q: "Favortie country",
-    a: "Italy",
-    q: "Favorite flavor ice cream",
-    a: "chocolate",
-    q: "Faviorte pizza style",
-    a: "supreme",
-    q: "Faviorte number",
-    a: "two",
-    q: "Faviorte resturant",
-    a: "The Beirut"
-};
-     var questionText = document.createTextNode(questionAndAnswer)
+
 
 
  
