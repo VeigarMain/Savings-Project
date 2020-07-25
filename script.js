540beed2387374f6a7bdf1c70a1ddeba894281f6
Kevin Lee <klee138@gmail.com>
2020-07-25 12:00:14 -0400

    sample pie chart with sample data

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-
-
-
- 
-        
-  
+var myChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: data {
+        labels: ["How much $ you saved", "How much $ until goal reached"],
+        datasets: [
+            {
+                label: "$",
+                data: [4500,10000]
+            }
+        ]
+    },
+    options: options
+});
