0726b7381692a25b56991b4d57140d384a86c940
Jake Battaglia <jbaked12@gmail.com>
2020-07-25 13:12:35 -0400

    input/buttons function with ajax call for giph

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-
-
+$(document).ready(function() {
+    // button on click function
+    $("#search-button").on("click", function () {
+        var searchTerm = $("#search-value").val();
+        $("#search-value").val("");
+        buttonFunction(searchTerm);
+        
+    })
+});
+    function buttonFunction(searchTerm) {
+        $.ajax({
+            type: "GET",
+            url: "api.giphy.com/v1/gifs/search?q=" + searchTerm + "A28hTTUxthLUB9TjNw2aZnUfAfLQQoeC",
+                        
+        })          
+     }
+     
+      
 
  
         
