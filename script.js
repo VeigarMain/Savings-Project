917ec9c71110e1190d6bc3dae96b6eaac2fe8f2c
Jake Battaglia <jbaked12@gmail.com>
2020-07-28 18:36:39 -0400

    new commit

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 $(document).ready(function() {
-    // button on click function
-    $("#search-button").on("click", function () {
-        var searchTerm = $("#search-value").val();
-        $("#search-value").val("");
+    
+    $("button").on("click", function () {
+        var searchTerm = $("#search-giphy").val();
+        $("#search-giphy").val("");
         buttonFunction(searchTerm);
-        
+        // alert("I am working!");
     })
-});
-    function buttonFunction(searchTerm) {
-        $.ajax({
-            type: "GET",
-            url: "api.giphy.com/v1/gifs/search?q=" + searchTerm + "A28hTTUxthLUB9TjNw2aZnUfAfLQQoeC",
-                        
-        })          
-     }
-     
-      
-
- 
         
+          
+    });
+
+function buttonFunction(searchTerm) {
+    var apiKey = "A28hTTUxthLUB9TjNw2aZnUfAfLQQoeC";
+       $.get(
+        "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=" + apiKey +"&limit=1",
+         function(data)
+         { 
+             var info = Object.values(data)[0]
+             console.log("succes got data", info.length);
+             for (var i = 0; i < info.length; i++) {
+                 var img = $("<img>").addClass("img" + 1).attr("src", info[i].images.original.url);
+                 $("p").append(img);
+             }
+           }
+       );          
+};
   
