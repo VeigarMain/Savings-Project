58cd4512c8b821ef9308cc42215551667a73b3e7
Kevin Lee <klee138@gmail.com>
2020-07-28 20:36:49 -0400

    revised chart and localstorage data

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,27 @@
 
- 
+
 //var remain = cost - saved
 
-$(document).ready(function() {
+$(document).ready(function () {
     // button on click function
     $("#search-button").on("click", function () {
         var searchTerm = $("#search-value").val();
         $("#search-value").val("");
         buttonFunction(searchTerm);
-        
+
     })
-    
+
     function buttonFunction(searchTerm) {
         $.ajax({
             type: "GET",
             url: "api.giphy.com/v1/gifs/search?q=" + searchTerm + "A28hTTUxthLUB9TjNw2aZnUfAfLQQoeC",
-            
-        })          
-     }
-     $("#costBtn").on("click", function () {
+
+        })
+    }
+    $("#costBtn").on("click", function () {
         var cost = $("#cost").val();
         console.log(cost);
-        
+
         //$("#cost").val("");
         //costFunction();
 
@@ -30,48 +30,64 @@ $(document).ready(function() {
         //var remain = goal - saved;
         //console.log(remain);
     })
-    
-    $("#savedBtn").on("click", function () {
-        var saved = $("#saved").val();
-        console.log(saved);
-        
-        $("#saved").val("");
-        savedFunction();
-        localStorage.setItem('savedAmt', saved);
-        function savedFunction(savedAmt) {
-   
-        }
-     })
-     
 
-     function chartDataFunction() {
-         var goal = localStorage.getItem("goalAmt");
-         console.log(goal);
-         var totalSaved = saved + localStorage.getItem("savedAmt");
-         var remainingAmt = goal - totalSaved;
-                     
-         new Chart(document.getElementById("doughnut-chart"), {
-                         type: 'doughnut',
-                         data: {
-                             labels: ["How much $ you saved", "How much $ until goal reached"],
-                             datasets: [
-                                 {
-                                     label: "$",
-                                     backgroundColor: ["#3e95cd", "#8e5ea2"],
-                                     data: [saved, remainingAmt]
-                                     //data: [4500,10000]
-                                 }
-                             ]
-                         },
-                         options: {
-                     
-                         }
-                     });
-         //input saved amount into doughnut chart object data array
-         //saved amount should be a running total and saved in localstorage
-         //calculate amount left to goal by subtracting total saved from goal cost
-      //  (data).push
-}});
+    $("#savedBtn").on("click", function () {
+        var saved = parseInt($("#saved").val());
+
+        if (!saved) {
+            saved = 0;
+        }
+
+
+        var goal = localStorage.getItem("goalAmt");
+
+        var savedAmount = parseInt(localStorage.getItem('savedAmt'));
+        if (!savedAmount) {
+            savedAmount = 0
+        }
+
+        var totalSaved = parseInt(saved) + parseInt(savedAmount);
+
+        var remainingAmt = parseInt(goal) - parseInt(totalSaved);
+        var chartData = [totalSaved, remainingAmt];
+
+
+        $("#saved").val("");
+        savedFunction(chartData);
+
+        localStorage.setItem('savedAmt', totalSaved);
+
+    })
+
+
+    //input saved amount into doughnut chart object data array
+    //saved amount should be a running total and saved in localstorage
+    //calculate amount left to goal by subtracting total saved from goal cost
+    //  (data).push
+    function savedFunction(DATA) {
+
+        new Chart(document.getElementById("doughnut-chart"), {
+            type: 'doughnut',
+            data: {
+                labels: ["How much $ you saved", "How much $ until goal reached"],
+                datasets: [
+                    {
+                        label: "$",
+                        backgroundColor: ["#3e95cd", "#8e5ea2"],
+                        //data: [saved, remainingAmt]
+                        data: DATA
+                    }
+                ]
+            },
+            options: {
+
+            }
+        });
+
+    }
+
+    savedFunction([1, 1]);
+});
 
 /* var myChart = new Chart(ctx, {
     type: 'doughnut',
